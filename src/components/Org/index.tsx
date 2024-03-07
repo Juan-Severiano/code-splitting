c00fd90cd61ddd1b2794111162271aecec962ebb
Francisco Juan <juansoussev@gmail.com>
2024-03-07 14:08:22 -0300

    feat lazy components created

---
+++ src/components/Org/index.tsx
@@ -0,0 +1,10 @@
+function Org() {
+  return (
+    <>
+      <h3>The org name</h3>
+      <p>Teste one two trhee</p>
+    </>
+  )
+}
+
+export default Org
---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <>
+      <h3>The username</h3>
+      <p>Luis</p>
+    </>
+  )
+}
+
+export default Profile
